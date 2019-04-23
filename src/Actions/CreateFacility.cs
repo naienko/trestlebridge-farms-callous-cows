60d06396c36b2eb07a01659925f386736d36bb97
Panya Farnette <naienko@gmail.com>
2019-04-23 15:46:42 -0500

    all main menu entries repeat menu on invalid entry

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,44 +3,60 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class CreateFacility {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
-            Console.WriteLine ("3. Natural field");
-            Console.WriteLine ("4. Chicken house");
-            Console.WriteLine ("5. Duck house");
+namespace Trestlebridge.Actions
+{
+	public class CreateFacility
+	{
+		public static void CollectInput(Farm farm)
+		{
+			Console.WriteLine("1. Grazing field");
+			Console.WriteLine("2. Plowed field");
+			Console.WriteLine("3. Natural field");
+			Console.WriteLine("4. Chicken house");
+			Console.WriteLine("5. Duck house");
 
-            Console.WriteLine ();
-            Console.WriteLine ("Choose what you want to create");
+			Console.WriteLine();
+			Console.WriteLine("Choose what you want to create");
 
-            Console.Write ("> ");
-            string input = Console.ReadLine ();
-
-            switch (Int32.Parse(input))
-            {
-                case 1:
-                    farm.AddGrazingField(new GrazingField());
-                    break;
-                case 2:
-                    farm.AddPlowedField(new PlowedField());
-                    break;
-                case 3:
-                    farm.AddNaturalField(new NaturalField());
-                    break;
-                case 4:
-                    farm.AddChickenHouse(new ChickenHouse());
-                    break;
-                case 5:
-                    farm.AddDuckHouse(new DuckHouse());
-                    break;
-                default:
-                    Console.WriteLine($"Invalid option: {input}");
-                    Console.WriteLine("Press any key to go back to main menu.");
-                    Console.ReadLine();
-                    break;
-            }
-        }
-    }
+			Console.Write("> ");
+			
+			try
+			{
+				int choice = Int32.Parse(Console.ReadLine());
+				switch (choice)
+				{
+					case 1:
+						farm.AddGrazingField(new GrazingField());
+						break;
+					case 2:
+						farm.AddPlowedField(new PlowedField());
+						break;
+					case 3:
+						farm.AddNaturalField(new NaturalField());
+						break;
+					case 4:
+						farm.AddChickenHouse(new ChickenHouse());
+						break;
+					case 5:
+						farm.AddDuckHouse(new DuckHouse());
+						break;
+					default:
+						Console.WriteLine($"Invalid option: {choice}");
+						Console.WriteLine("Press any key to go back to the menu.");
+						Console.ReadLine();
+                        Console.Clear();
+                        CreateFacility.CollectInput(farm);
+						break;
+				}
+			}
+			catch (FormatException)
+			{
+                Console.WriteLine($"Invalid option");
+                Console.WriteLine("Press any key to go back to the menu.");
+                Console.ReadLine();
+                Console.Clear();
+                CreateFacility.CollectInput(farm);
+			}
+		}
+	}
 }
--- src/Actions/PurchaseSeeds.cs
+++ src/Actions/PurchaseSeeds.cs
@@ -4,48 +4,73 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Plants;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseSeeds {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Sesame");
-            Console.WriteLine ("2. Sunflower");
-            Console.WriteLine ("3. Wildflower");
+namespace Trestlebridge.Actions
+{
+	public class PurchaseSeeds
+	{
+		public static void CollectInput(Farm farm)
+		{
+			Console.WriteLine("1. Sesame");
+			Console.WriteLine("2. Sunflower");
+			Console.WriteLine("3. Wildflower");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+			Console.WriteLine();
+			Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
-            Console.Clear();
-            switch (Int32.Parse(choice))
-            {
-                case 1:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
-                    break;
-                case 2:
-					Console.Clear();
-					Console.WriteLine ("1. Seeds");
-					Console.WriteLine ("2. Compost");
-					Console.WriteLine ();
-					Console.WriteLine ("What resource do you want from Sunflowers?");
-					Console.Write ("> ");
-					int resourceChoice = Int32.Parse(Console.ReadLine ());
-					if (resourceChoice == 1) {
-                    	ChoosePlowedField.CollectInput(farm, new Sunflower());
-					} else if (resourceChoice == 2) {
-						ChooseNaturalField.CollectInput(farm, new Sunflower());
-					} else {
-						Console.WriteLine($"Invalid option: {resourceChoice}");
-                    	Console.WriteLine("Press any key to go back to main menu.");
-                    	Console.ReadLine();
-					}
-                    break;
-                case 3:
-                    ChooseNaturalField.CollectInput(farm, new Wildflower());
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+			Console.Write("> ");
+
+			try
+			{
+				int choice = Int32.Parse(Console.ReadLine());
+				Console.Clear();
+				switch (choice)
+				{
+					case 1:
+						ChoosePlowedField.CollectInput(farm, new Sesame());
+						break;
+					case 2:
+						Console.Clear();
+						Console.WriteLine("1. Seeds");
+						Console.WriteLine("2. Compost");
+						Console.WriteLine();
+						Console.WriteLine("What resource do you want from Sunflowers?");
+						Console.Write("> ");
+						int resourceChoice = Int32.Parse(Console.ReadLine());
+						if (resourceChoice == 1)
+						{
+							ChoosePlowedField.CollectInput(farm, new Sunflower());
+						}
+						else if (resourceChoice == 2)
+						{
+							ChooseNaturalField.CollectInput(farm, new Sunflower());
+						}
+						else
+						{
+							Console.WriteLine($"Invalid option: {resourceChoice}");
+							Console.WriteLine("Press any key to go back to main menu.");
+							Console.ReadLine();
+						}
+						break;
+					case 3:
+						ChooseNaturalField.CollectInput(farm, new Wildflower());
+						break;
+					default:
+                        Console.WriteLine($"Invalid option: {choice}");
+						Console.WriteLine("Press any key to go back to the menu.");
+						Console.ReadLine();
+                        Console.Clear();
+                        CollectInput(farm);
+						break;
+				}
+			}
+			catch (FormatException)
+			{
+                Console.WriteLine($"Invalid option");
+				Console.WriteLine("Press any key to go back to the menu.");
+				Console.ReadLine();
+                Console.Clear();
+                CollectInput(farm);
+			}
+		}
+	}
 }
