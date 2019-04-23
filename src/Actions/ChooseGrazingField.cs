fe5d27d3a251249d8f8af7340a22f66e00117909
Panya Farnette <naienko@gmail.com>
2019-04-23 10:26:40 -0500

    facilities listing includes number of type of animals

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Linq;
+using System.Text;
+using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
+using Trestlebridge.HashBox;
 
 namespace Trestlebridge.Actions {
     public class ChooseGrazingField {
@@ -10,11 +13,32 @@ namespace Trestlebridge.Actions {
             foreach (GrazingField field in farm.GrazingFields)
             {
                 if (field.Animals.Count < field.Capacity) {
-                    Console.WriteLine ($"{farm.GrazingFields.IndexOf(field)+1}. Plowed Field ({field.Animals.Count} of {field.Capacity} animals)");
+                    StringBuilder output = new StringBuilder();
+                    output.Append($"{farm.GrazingFields.IndexOf(field)+1}. Grazing Field (");
+                    if (field.Animals.Count == 0) {
+                        output.Append("0");
+                    } else {
+                        //group by
+                        List<CountAnimals> animalCount = (
+                            from ruminant in field.Animals
+                            group ruminant by ruminant.Type into AnimalGroup
+                            select new CountAnimals {
+                                Type = AnimalGroup.Key,
+                                Count = AnimalGroup.Count()
+                            }
+                        ).ToList();
+                        foreach (CountAnimals entry in animalCount) {
+                            // TODO: remove trailing comma
+                            output.Append($"{entry.Count} {entry.Type},");
+                        }
+                    }
+                    output.Append($" of {field.Capacity} animals)");
+                    Console.WriteLine(output);
                 }
             }
             // STRETCH: realign numbers for facility listing when it skips full ones
             // put available fields in a new temporary List<>? then foreach the list?
+            // use linq directives to pull available fields?
 
             Console.WriteLine ();
 
---
+++ src/HashBox/CountAnimals.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Trestlebridge.HashBox {
+	public class CountAnimals {
+		public string Type { get; set; }
+		public int Count { get; set; }
+	}
+}
