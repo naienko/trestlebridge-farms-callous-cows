680e7917267bafaa515fee2767d02a090e8babc1
Panya Farnette <naienko@gmail.com>
2019-04-24 13:54:08 -0500

    why isn't my List Materials building up with Dictionary _materials

--- src/Actions/Producers/ChooseSeedProducer.cs
+++ src/Actions/Producers/ChooseSeedProducer.cs
@@ -51,34 +51,35 @@ namespace Trestlebridge.Actions.Producers
 			//IResource<SeedProcessor> chosenSeed = chosenField.Plants[resourceChoice - 1];
 			IResource<SeedProcessor> chosenSeed = chosenField.Plants.First(n => n.Type == plantCount[resourceChoice - 1].Type);
 
-			Console.WriteLine($"How many {plantCount[resourceChoice - 1].Type} should be processed?");
+			Console.WriteLine($"How many {chosenSeed.Type} should be processed?");
 			Console.Write("> ");
 			//break out to new file?
 			int resourceCount = Int32.Parse(Console.ReadLine());
 			SeedProcessor _seedProcessor = new SeedProcessor();
 			if (resourceCount <= _seedProcessor.Capacity) //change this check to account for all _seedProcessor.Materials
-			//use reduce to find out how many total rows are in _seedProcessor.Materials
+														  //use reduce to find out how many total rows are in _seedProcessor.Materials
 			{
+				Dictionary<int, IResource<SeedProcessor>> _material = new Dictionary<int, IResource<SeedProcessor>>();
+				_material.Add(resourceCount, chosenSeed);
+				_seedProcessor.Materials.Add(_material);
+				for (int i = 0; i <= resourceCount; i++)
+				{
+					if (chosenField.Plants[i] == chosenSeed)
+					{
+						chosenField.Plants.RemoveAt(i);
+					}
+				}
 				Console.WriteLine("Ready to process? (Y/n)");
 				Console.Write("> ");
 				string processGo = Console.ReadLine();
 				if (processGo == "y")
 				{
-					Dictionary<int, IResource<SeedProcessor>> _material = new Dictionary<int,IResource<SeedProcessor>>();
-					_material.Add(resourceCount, chosenSeed);
-					_seedProcessor.Materials.Add(_material);
-					for (int i = 0; i <= resourceCount; i++)
-					{
-						if (chosenField.Plants[i] == chosenSeed) {
-							chosenField.Plants.RemoveAt(i);
-						}
-					}
-
 					foreach (Dictionary<int, IResource<SeedProcessor>> material in _seedProcessor.Materials)
 					{
 						foreach (KeyValuePair<int, IResource<SeedProcessor>> item in material)
 						{
-							for (int i = 0; i <= item.Key; i++)
+							Console.WriteLine($"processing {item.Key} {item.Value.Type}");
+							for (int i = 0; i < item.Key; i++)
 							{
 								Dictionary<IResource<SeedProcessor>, double> _output = new Dictionary<IResource<SeedProcessor>, double>();
 								_output.Add(item.Value, item.Value.Process(_seedProcessor));
