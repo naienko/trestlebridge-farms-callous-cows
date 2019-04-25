4fc003d7285ee6ec5b8b5c72bb9a36fa26943fc6
Panya Farnette <naienko@gmail.com>
2019-04-25 09:56:32 -0500

    this path doesn't work, store for reference

--- src/Actions/Producers/ChooseSeedProducer.cs
+++ src/Actions/Producers/ChooseSeedProducer.cs
@@ -23,11 +23,11 @@ namespace Trestlebridge.Actions.Producers
 
 			Console.WriteLine();
 			Console.WriteLine("Which facility has the plants you want to process?");
-//ask for input
+			//ask for input
 			Console.Write("> ");
 			//acquire input
 			int choice = Int32.Parse(Console.ReadLine());
-//use input to fill chosen field object variable 
+			//use input to fill chosen field object variable 
 			PlowedField chosenField = farm.PlowedFields[choice - 1];
 			Console.Clear();
 			//create hashset of plants by type with count
@@ -46,25 +46,23 @@ namespace Trestlebridge.Actions.Producers
 				Console.WriteLine($"{plantCount.IndexOf(entry) + 1}. {entry.Count} {entry.Type}");
 			}
 			Console.WriteLine();
-//ask for input
+			//ask for input
 			Console.WriteLine("Which resource should be processed?");
 			Console.Write("> ");
 			//acquire input
 			int resourceChoice = Int32.Parse(Console.ReadLine());
-			// this doesn't work, because of the way the hashset works
-			//IResource<SeedProcessor> chosenSeed = chosenField.Plants[resourceChoice - 1];
-			//
+
 			//use input to fill chosen plant type variable by getting the first object from the list of plant objects in the chosen field that matches the chosen type in the hashset
 			IResource<SeedProcessor> chosenSeed = chosenField.Plants.First(n => n.Type == plantCount[resourceChoice - 1].Type);
-//ask for input
+			//ask for input
 			Console.WriteLine($"How many {chosenSeed.Type} should be processed?");
 			Console.Write("> ");
 			//break out to new file?
 			//acquire input
 			int resourceCount = Int32.Parse(Console.ReadLine());
-			
+
 			if (resourceCount <= equipment.Capacity) //change this check to account for all equipment.Materials
-														  //use reduce to find out how many total rows are in equipment.Materials
+													 //use reduce to find out how many total rows are in equipment.Materials
 			{
 				//create new Material object with chosen plant type and number of plants to process
 				Material<SeedProcessor> _material = new Material<SeedProcessor>()
@@ -73,16 +71,31 @@ namespace Trestlebridge.Actions.Producers
 					Count = resourceCount
 				};
 				//add Material object to List in machine object
-				//why are the Material objects overwriting each other
 				equipment.Materials.Add(_material);
 				//loop through the list of plant objects in the chosen field object and remove #resourceCount objects from that list iff they match the chosen plant type
 				//TODO: this loop doesn't work right -- it's only removing 1, not resourceCount
+				//I want a loop that doesn't increment until the iff fails
+				//this loop doesn't loop through the whole list of plant objects
+
+
 				for (int i = 0; i < resourceCount; i++)
 				{
-					if (chosenField.Plants[i] == chosenSeed)
+					Console.WriteLine($"resource index is {i}");
+					for (int j = 0; j < chosenField.Plants.Count; j++)
 					{
-						chosenField.Plants.RemoveAt(i);
+						Console.WriteLine($"field index is {j}");
+						while (chosenField.Plants[j].Type == chosenSeed.Type)
+						{
+							Console.WriteLine($"There is a match at index {j}");
+							chosenField.Plants.RemoveAt(j);
+						}
+						// else
+						// {
+						// 	Console.WriteLine($"There is NOT a match at index {j}");
+						// }
+						Console.WriteLine($"increment field index {j}");
 					}
+					Console.WriteLine($"increment resource index {i}");
 				}
 				//ask for input
 				Console.WriteLine("Ready to process? (Y/n)");
@@ -107,7 +120,7 @@ namespace Trestlebridge.Actions.Producers
 							equipment.Output.Add(_output);
 						}
 					}
-//loop through the list of output Dictionaries
+					//loop through the list of output Dictionaries
 					foreach (Dictionary<IResource<SeedProcessor>, double> output in equipment.Output)
 					{
 						//iterate each keyvaluepair in chosen Dictionary
